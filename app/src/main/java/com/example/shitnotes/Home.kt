df16b96d23ccc7ae3ed7c1a1e3f60de113d26f70
Jason Loi <z5420621@ad.unsw.edu.au>
2024-08-27 18:54:25 +1000

    Prototype home screen + navrouter

---
+++ app/src/main/java/com/example/shitnotes/Home.kt
@@ -0,0 +1,261 @@
+package com.example.shitnotes
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun Home(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .safeContentPadding(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HomeNavBar()
+        HomeViewSwitchBar()
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Workout Log Empty", fontSize = 26.sp, color = Color.Gray)
+        }
+
+        HomeBlankButtons()
+    }
+}
+
+@Composable
+fun HomeNavBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.DarkGray)
+            .height(56.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        ) {
+            /*TODO: Add logo*/
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = "Logo",
+                tint = Color.White,
+                modifier = Modifier.size(32.dp)
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Text(
+                text = "ShitNotes",
+                color = Color.White,
+                fontSize = 18.sp,
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                ),
+                contentPadding = PaddingValues(1.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Calendar",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                ),
+                contentPadding = PaddingValues(1.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "AddExercise",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                ),
+                contentPadding = PaddingValues(1.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Settings",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeViewSwitchBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+            .height(56.dp)
+    ) {
+        Button(
+            onClick = {
+                /*TODO: Map arrow to previous Home*/
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color(0xFFCF7251)
+            ),
+            contentPadding = PaddingValues(0.dp),
+            modifier = Modifier.padding(4.dp),
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowLeft,
+                contentDescription = "Left",
+                modifier = Modifier.size(36.dp)
+            )
+        }
+
+        /*TODO: Set text to variable*/
+        Text(
+            text = "Today".uppercase(),
+            color = Color.White,
+            fontSize = 18.sp,
+        )
+
+        Button(
+            onClick = {
+                /*TODO: Map arrow to next Home*/
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color(0xFFCF7251)
+            ),
+            contentPadding = PaddingValues(0.dp),
+            modifier = Modifier.padding(4.dp),
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
+                contentDescription = "Right",
+                modifier = Modifier.size(36.dp)
+            )
+        }
+    }
+
+    HorizontalDivider(
+        color = Color(0xFFCF7251),
+        thickness = 4.dp,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun HomeBlankButtons() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 32.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "StartNewWorkout",
+                    modifier = Modifier.size(32.dp),
+                    tint = Color(0xFFCF7251)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Start New Workout", fontSize = 16.sp, color = Color.Gray)
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                    contentDescription = "CopyPreviousWorkout",
+                    modifier = Modifier.size(32.dp),
+                    tint = Color(0xFFCF7251)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Copy Previous Workout", fontSize = 16.sp, color = Color.Gray)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shitnotes/NavRouter.kt
@@ -0,0 +1,15 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.shitnotes.Home
+
+@Composable
+fun NavRouter() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            Home(navController)
+        }
+    }
+}
